1decb03e4b630131622ff481e61ac15e3fe0a711
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-05-07 08:51:40 +0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MemQu
+//
+//  Created by Atikah Aulia on 02/05/21.
+//
+
+import UIKit
+import Core
+
+class ViewController: UIViewController {
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        let appdelegate = U
+//        let context = appdelegate.
+
+    }
+    
+//    func getAllHafalanItems(){
+//        let hafalanItems = context.fetch()
+//    }
+//
+//    func getAllMurajaahItems(){
+//
+//    }
+//
+//    func addHafalan(tanggalHafalan: Date, surahHafalan: String, ayatAwalHafalan: Int32, ayatAkhirHafalan: Int32 ){
+//
+//    }
+//
+//    func addMurajaah(tanggalMurajaah: Date, surahMurajaah: String, ayatAwalMurajaah: Int32, ayatAkhirMurajaah: Int32){
+//
+//    }
+//
+}
+
